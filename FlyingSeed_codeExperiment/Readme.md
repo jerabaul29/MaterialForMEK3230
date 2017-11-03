474d6171d31de4f3fb22661b905788dba27f7e07
jerabaul29 <jean.rblt@gmail.com>
2017-11-03 10:39:22 +0100

    modified analyze_collected_data.py

--- FlyingSeed_codeExperiment/Readme.md
+++ FlyingSeed_codeExperiment/Readme.md
@@ -5,3 +5,11 @@ The data can be quite heavy. Put all the videos on an external hard disk that yo
 - **process_valid_range_data**: apply calibration on valid range data and extract mean vertical velocity and angular velocity.
 - **collect_all_data**: collect all data into one results folder on your own computer hard disk. This way, the heavy files are still on the external HDD but you have the light processed data locally.
 - **analyze_collected_data**: do additional processing of the collected data. This is also the file in which you will have to write your personal code and scripts.
+
+Note that you will have to adapt the code to your exact needs. In particular, you will have to:
+
+- set up the paths to correspond to your hard disks, computer folders tree etc.
+- set up the arguments of the *fnmatch* function calls to select the right folders
+- write your own code for plotting the data
+
+In case of doubt, go through the source code.
--- FlyingSeed_codeExperiment/analyze_collected_data.py
+++ FlyingSeed_codeExperiment/analyze_collected_data.py
@@ -3,9 +3,6 @@ import pickle
 from parse import *
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-
-%matplotlib inline
 
 path_to_data = '/home/hydroubuntu/Desktop/Current/data_seed_experiment_07122016/'
 path_save_figures = '/home/hydroubuntu/Desktop/Current/figures_seed_experiment_07122016/'
@@ -33,57 +30,13 @@ with open(path_to_data + 'dict_all_results.pkl', 'r') as crrt_file:
         dict_all_results = pickle.load(crrt_file)
 
 # parse the file names ---------------------------------------------------------
-dict_properties_case = {}
-list_wing_types = []
-list_weight_load = []
-list_repetition = []
 
-for crrt_case in list_cases:
+# you will have to write your own code!
+# note that, if you used correct path names, all the data generated is in: dict_all_results
+# you can look at what data is in dict_all_results using:
 
-    wing_type = parse("seed_{}_{}", crrt_case)[0]
-    if wing_type == 'thicker':
-        wing_type = 'thickest'
-    dict_properties_case[crrt_case + "_wing_type"] = wing_type
-    list_wing_types.append(wing_type)
-
-    weight_load = parse("seed_{}_{}lead{}", crrt_case)[1]
-    dict_properties_case[crrt_case + "_weight_load"] = int(weight_load)
-    list_weight_load.append(weight_load)
-
-    repetition = parse("{}_repetition{}.mkvDIR", crrt_case)
-    if repetition is not None:
-        dict_properties_case[crrt_case + "_repetition"] = repetition[1]
-    else:
-        dict_properties_case[crrt_case + "_repetition"] = '0'
-    list_repetition.append(dict_properties_case[crrt_case + "_repetition"])
-
-# put all the data in one pandas -----------------------------------------------
-df = pd.DataFrame(list_cases)
-df['wing_type'] = list_wing_types
-df['weight_load'] = list_weight_load
-df['repetition'] = list_repetition
-
-list_mean_vertical_velocity = []
-list_averaged_seed_rotation = []
-
-for crrt_case in list_cases:
-    list_mean_vertical_velocity.append(dict_all_results[crrt_case + "mean_vertical_velocity"])
-    list_averaged_seed_rotation.append(dict_all_results[crrt_case + "averaged_seed_rotation"])
-
-df['averaged_seed_rotation'] = list_averaged_seed_rotation
-df['mean_vertical_velocity'] = list_mean_vertical_velocity
-
-# compute some data on pandas columns ------------------------------------------
-df['pitch_angle'] = np.abs(df['averaged_seed_rotation'] / df['mean_vertical_velocity'])
-
-# just a few examples of how to use pands --------------------------------------
-
-# data frame
-df
-
-# how to extract all cases with wing_type being intermediate
-df[df['wing_type'] == 'intermediate']
-
-# do some plotting -------------------------------------------------------------
-
-# PUT YOUR CODE HERE!
+for crrt_key in dict_all_results.keys():
+    print("crrt_key: " + crrt_key)
+    
+# you want to use the data corresponding to the seed mean vertical velocity and
+# rotation frequency
